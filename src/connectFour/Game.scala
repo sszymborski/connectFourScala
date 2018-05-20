495aeabbaf5eae709afb3b9a60460b9801205929
szymon <S.Szymborski@stud.elka.pw.edu.pl>
2018-05-20 21:53:49 +0200

    In progress

--- src/connectFour/Game.scala
+++ src/connectFour/Game.scala
@@ -8,4 +8,17 @@ object Game extends App {
   //TODO przyjmowanie ruchów od gracza (gui.getMove(board))
   //TODO prośba o ruch do AI (AI.makeMove(board))
 
+
+  val board = Array.ofDim[Int](7, 6)
+  val whoPlays = true
+
+  def checkWin(): Boolean = {
+
+    for {i <- 1 to board.length
+         j <- 1 to board(0).length
+         if board(i)(j) != 0} yield
+      print(board(i)(j))
+    false
+  }
+
 }
